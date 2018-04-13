b017c27ab143fb09905a6a2fc9a991cd2c887119
Teuno Hooijer <teunohooijer@yahoo.com>
2018-04-13 11:35:52 +0200

    updated styling for summaryOfRoute

--- src/pages/normalroute/SummaryOfRoute.vue
+++ src/pages/normalroute/SummaryOfRoute.vue
@@ -3,28 +3,30 @@
     <h1 class="page-title">Summary of the route</h1>
 
     <div>
-      <p>Name of the route</p>
-      <p>{{name}}</p>
+      <p class="is-size-5 has-text-weight-bold">Name of the route</p>
+      <p class="is-size-7 has-text-weight-light">{{name}}</p>
     </div>
 
     <div>
-      <p>Kilometers walked</p>
-      <p>{{showDistanceFromMeterToKilometer}}</p>
+      <p class="is-size-5 has-text-weight-bold">Kilometers walked</p>
+      <p class="is-size-7 has-text-weight-light">{{showDistanceFromMeterToKilometer}}</p>
     </div>
 
     <div>
-      <p>Time walked</p>
-      <p>{{showTimeWalkedInHoursAndMinutes}}</p>
+      <p class="is-size-5 has-text-weight-bold">Time walked</p>
+      <p class="is-size-7 has-text-weight-light">{{showTimeWalkedInHoursAndMinutes}}</p>
     </div>
 
     <div>
-      <p>Did you like this route? Then rate us!</p>
+      <p class="is-size-5 has-text-weight-bold">Did you like this route? Then rate us!</p>
       <star-rating
         @rating-selected ="setRating"
         v-model="rating"
         v-bind:show-rating="false">
       </star-rating>
     </div>
+
+
   </div>
 </template>
 
@@ -67,13 +69,20 @@
 <style lang="scss" scoped>
   @import "../../assets/sass/main";
   .summary-route{
-    margin: 0;
-    width: 100vw;
+    padding: 2em;
     min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
 
     background-color: $green;
     color: $text_white;
-    font-family: $coolvetica_1;
-    font-size: 1.5em;
+    font-family: 'Francois One', sans-serif;
+    font-size: 1.2em;
   }
+
+  .page-title{
+    font-size: 1.8em;
+  }
+
 </style>
