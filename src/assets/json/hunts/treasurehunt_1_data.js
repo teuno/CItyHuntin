746edf5d84680708f26c4595d5ec53d9bf65513c
Teuno Hooijer <teunohooijer@yahoo.com>
2018-05-15 10:58:58 +0200

    merged with addVuex

---
+++ src/assets/json/hunts/treasurehunt_1_data.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    name: "Weapon factory",
+    description: "The building was created as a weapon store with it's own factory. Till the gun law of 1919 weapens were legel in The Netherlands. Heinrich Bremer and his son Theodoor were both real gunmakers theirselves. Becaue of the industrialization there was very little demand for hand made guns. At the second floor there was a warehouse for gunpouder, the permit of 1908 tells there was 37kg of gunpowder stored. The place was been a weapon place till World War II ",
+    date_build: "1887",
+    image_url: "",
+    position: {lat: 52.511223  , lng: 6.090891 },
+    challengeTitle: '',
+    challengeQuestion: '',
+    challengeCompleteHint: '',
+  },
+  {
+    name: "Farmacy",
+    description: "The building with it's charistic art above the door is much older than the buildings around it. When the St. Michaëlstower burned down in 1669 some of the buildings in the Luttekestraat, even when the remaining pieces fell down in the Luttekestraat and this building survied. Some say it's luck some say the Holy Mother statue in the back garden protects the building, for a long time there used to be a farmacy till the 21th century" ,
+    date_build: "1609",
+    image_url: "",
+    position: {lat: 52.512037 , lng: 6.091195},
+    challengeTitle: '',
+    challengeQuestion: '',
+    challengeCompleteHint: '',
+  },
+];
+
+export default data;
